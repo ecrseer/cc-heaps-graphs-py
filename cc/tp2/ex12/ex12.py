e5f3d42d5d234db4e52c435df9c4109d1b4047fd
Gabriel Couto <gabriel.justino@dsgtechnology.com.br>
2025-03-01 17:15:47 -0300

    media cython

--- cc/tp2/ex12/ex12.py
+++ cc/tp2/ex12/ex12.py
@@ -1,29 +1,34 @@
 import time
 
-from vector import somar_vet_aleatorio,somar_vet_linear;
+from vector import somar_vet_aleatorio, somar_vet_linear;
 
+
+def realizar_somas():
+    print("Somando vetor aleatório")
+    inicio = time.time()
+    soma = somar_vet_aleatorio()
+    demorou_aleatorio = time.time() - inicio
+
+    print("\n---\nSomando vetor linear")
+    inicio_linear = time.time()
+    soma_linear = somar_vet_linear()
+    demorou_linear = time.time() - inicio_linear
+    return demorou_aleatorio, demorou_linear
 
 
 def ex12():
-    print("Exercício 1.2: Cálculo Paralelo com OpenMP (simulação com multiprocessing)")
-    print("Somando vetor aleatório")
-    inicio = time.time()
-    soma=somar_vet_aleatorio()
-    # print(f"Vetor: {vet}")
-    print(f"Soma: {soma}")
-    demorou = time.time() - inicio
-    print(f"Demorou {demorou:.2f} segundos")
+    aleatorio_media = 0
+    linear_media = 0
+    for i in range(10):
+        aleatorio_ms, linear_ms = realizar_somas()
+        aleatorio_media += aleatorio_ms
+        linear_media += linear_ms
 
-    print("\n---\nSomando vetor linear")
-    inicio = time.time()
-    soma = somar_vet_linear()
-    # print(f"Vetor: {vet2}")
-    print(f"Soma: {soma}")
-    demorou = time.time() - inicio
-    print(f"Demorou {demorou:.2f} segundos")
+    aleatorio_media /= 10
+    linear_media /= 10
+    print(f"\nMédia de tempo para somar vetor aleatório: {aleatorio_media:.2f} milisegundos")
+    print(f"Média de tempo para somar vetor linear: {linear_media:.2f} milisegundos")
 
 
 if __name__ == '__main__':
     ex12()
-
-
