04e36030f733e0a63578bdf6b33a36b4827f34fc
Gabriel Couto <gabriel.justino@dsgtechnology.com.br>
2025-03-16 23:31:21 -0300

    udp socket python fix

---
+++ redes/tp3/exercicio8.py
@@ -0,0 +1,12 @@
+import socket
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 6789
+address = (SERVER_IP ,SERVER_PORT)
+socket_client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+message = "Ola INFNET"
+socket_client.sendto(bytes(message,encoding='utf8'),address)
+response_server,addr = socket_client.recvfrom(4096)
+print("Response from the server => %s" % response_server.decode())
+
+socket_client.close()
